41ba0bc4267f836edd56abb2348f787d98ee82d1
HYEJIN KWON <chahaa0189@gmail.com>
2025-02-25 19:48:58 +0900

    8th commit

--- main.js
+++ main.js
@@ -1,165 +1,42 @@
 
-//     const numbers = new Array(45).fill().map((a, i) => i+1 );
+//1. Input에 값을 입력 했을 때, 값 넘어가게 하기
+//2. 버튼 눌렀을 때, 숫자가 6개가 들어가 있지 않으면 경고문 뜨게 하기
+//3. 숫자가 1~45까지만 입려 가능하게 하기
+//4. 숫자 입력 후, 당첨 숫자 및 보너스 숫자가 공안에 들어가게 하기
+
+let inputArea = document.getElementById("input-value")
+let randomBtn = document.getElementById("random-btn")
+
+randomBtn.addEventListener("click",()=>{
+
+    let inputValue = inputArea.value
+    let history = []
+   if(!inputValue.trim()){
+    return alert("숫자를 입력하세요.")
+   }
+   let numbers = inputValue.split(",").map((item)=>item.trim())
+   if(numbers.length !==6){
+    return alert("숫자를 6개 입력하세요")
+   }
    
-//     let selectNum = [];
+   if(new Set(numbers).size !== 6){
+    return alert("중복된 숫자입니다")
+   }
+   if(numbers.find((item)=> item>45 || item<1)){
+    return alert("1부터 45까지의 숫자를 입력하세요.")
+   }
 
-// for(let i = 1; i < 8; i++){ // 보너스 숫자까지 해야하니 7개가 나와야함
-//     let number = Math.floor(Math.random() * numbers.length);
-//     let randomNum = numbers.splice(number, 1);
-//     selectNum.push(randomNum);
-// }
+   let randomList = Array(45).fill().map((item,index)=>index+1)
+   let selectNum = []
 
-//   console.log(selectNum);
-//   const bonusNum = selectNum.splice(6, 1); // 보너스 숫자
-//   const winBalls = selectNum.sort((a ,b) => a-b); // 당첨 숫자
-
-//   for(let i = 0; i < winBalls.length; i++){
-//     setTimeout(() => {
-//         const ball = document.createElement('div');
-//         ball.className = 'ball';
-//         ball.textContent = winBalls[i];
-//         result.append(ball);
-//     }, 1000 * (i +1));
-//   }
-
-const $form = document.querySelector("#form");
-const $result = document.querySelector("#result");
-const $bonus = document.querySelector("#bonus");
-
-
-function colorize(number,$tag){
-    if(number<10){
-        $tag.style.color = 'white'
-           $tag.style.backgroundColor = "red"
-    }else if(number<20){
-        $tag.style.backgroundColor = "orange"
-    }else if(number<30){
-        $tag.style.backgroundColor = "yellow"
-    }else if(number<40){
-        $tag.style.backgroundColor = "blue"
-        $tag.style.color = 'white'
-    }else{
-         $tag.style.backgroundColor = "green"
-        $tag.style.color = 'white'
-
-    }
-}
+   for(let i=1;i<8;i++){
+        let randomNum = Math.floor(Math.random()*randomList.length)
+        console.log(randomNum)
+   }
 
 
 
-function drawBall(number,$parent){
-    const $ball = document.createElement("div")
-    $ball.className = "ball"
-    colorize(number,$ball)
-    $ball.textContent = number
-    $parent.appendChild($ball)
-
-}
-
-const setTimeoutPromise = (ms)=> new Promise((resolve,reject)=>{
-
-    setTimeout(resolve,ms)
-}) 
-
-
-let clickable = true;
-
-
-$form.addEventListener("submit",async (event) =>{
-    event.preventDefault();
-
-    if(!clickable){
-        return;
-    }
-clickable = false
-$result.innerHTML = "당첨 숫자 :"
-$bonus.innerHTML = "보너스 숫자 :"
-
-
-
-
-    const string = event.target.input.value;
-    if(!string.trim()){
-        return alert("숫자를 입력하세요.")
-    }
-    const myNumbers = string.split(',').map((v)=> parseInt(v.trim()))
-    if(myNumbers.length !== 6){
-        return alert("숫자를 6개 입력하시요.")
-    }
-   
-    if(new Set(myNumbers).size !== 6){
-        return alert("중복된 숫자입니다.")
-    }
-    if(myNumbers.find((v)=>v >45 || v<1)){
-        return alert("1부터 45까지의 값만 입력할 수 있습니다..")
-    }
-
-
-    const candidate = Array(45).fill().map((v,i) => i+1);
-    const shuffle = []
-    while(candidate.length>0){ 
-        const random =  Math.floor(Math.random()*candidate.length)
-        const spliceArray = candidate.splice(random,1)
-        const value = spliceArray[0]
-        shuffle.push(value)
-    }
-    console.log(shuffle)
-    const winBalls = shuffle.slice(0,6).sort((a,b)=>a-b)
-    const bonus = shuffle[6]
-    console.log(winBalls,bonus)   
-    
-
-
-
-
-
-for(let i=0; i<winBalls.length;i++){
-    await setTimeoutPromise(1000)
-      drawBall(winBalls[i],$result)
-        
-}
-    await setTimeoutPromise(1000)
-    drawBall(bonus,$bonus)
-
-await setTimeoutPromise(0)
-
-let count =0;
-myNumbers.forEach((my)=>{
-if(winBalls.includes(my)){
-    count++
-}
 })
 
-if(count ===6){
-    alert("1등 로또 당첨을 축하드립니다!")
-}else if(count ===5){
-    if(myNumbers.includes(bonus)){
-        alert("보너스 공으로 2등 로또 당첨을 축하드립니다! ")
-    }else{
-        alert("3등 로또 당첨을 축하드립니다!")
-    }
-}else if(count ===4){
-    alert("4등 5만원 당첨을 축하드립니다!")
-}else if(count ===3){
-    alert("5등 5천원 당첨을 축하드립니다!")
-}else{
-    alert("낙첨입니다.")
-}
 
 
-clickable = true;
-
-
-  
-});
-
-
-
-
-
-
-
-
-
-
-  
